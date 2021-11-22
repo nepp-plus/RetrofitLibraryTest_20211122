4b17d91bda8153a2ac6df4e7fadb91d3ba2e484c
nepp-plus <nepp.plus@gmail.com>
2021-11-22 18:01:34 +0900

    로그인 응답 처리  - 자동 파싱된 BasicResponse 변수 이용.

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.retrofitlibrarytest_20211122
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPI
 import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPIService
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
+import com.neppplus.retrofitlibrarytest_20211122.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -25,7 +30,23 @@ class MainActivity : BaseActivity() {
             val inputEmail = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
 
-            apiService.postRequestLogin(inputEmail, inputPw)
+            apiService.postRequestLogin(inputEmail, inputPw).enqueue( object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    val basicResponse =  response.body()!!
+
+                    Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            } )
 
 
         }
