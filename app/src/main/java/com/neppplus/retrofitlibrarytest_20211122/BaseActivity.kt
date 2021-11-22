7a7c3c820792995434e6f36a3f351aa13a9c0d76
nepp-plus <nepp.plus@gmail.com>
2021-11-22 17:51:13 +0900

    실제 로그인 API 호출 세팅   - 서버 접속 기능은 모든 화면에서 필요로 함.   - BaseActivity에 멤버변수로 만들어서 상속.

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/BaseActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/BaseActivity.kt
@@ -3,15 +3,23 @@ package com.neppplus.retrofitlibrarytest_20211122
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPI
+import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPIService
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+    lateinit var apiService : ServerAPIService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
+
     }
 
     abstract fun setupEvents()
--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
@@ -3,6 +3,8 @@ package com.neppplus.retrofitlibrarytest_20211122
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPI
+import com.neppplus.retrofitlibrarytest_20211122.api.ServerAPIService
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
@@ -17,6 +19,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnLogin.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+            apiService.postRequestLogin(inputEmail, inputPw)
+
+
+        }
 
     }
 
