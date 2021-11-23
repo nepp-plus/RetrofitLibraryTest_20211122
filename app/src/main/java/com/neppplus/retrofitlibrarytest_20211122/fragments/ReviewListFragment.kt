4a6007c3bf1153dea684c2a5217d80be1e3e1268
nepp-plus <nepp.plus@gmail.com>
2021-11-23 17:08:39 +0900

    리뷰 목록 프래그먼트 추가   - 뷰페이저 추가 중 (필요 프래그먼트 제작)

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/ReviewListFragment.kt
@@ -0,0 +1,27 @@
+package com.neppplus.retrofitlibrarytest_20211122.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.retrofitlibrarytest_20211122.R
+
+class ReviewListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_review_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+}
