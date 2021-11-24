ad9e98b0d11bb4ba5c7a753f2d6c8636d3099823
nepp-plus <nepp.plus@gmail.com>
2021-11-24 10:52:33 +0900

    내프로필 프래그먼트 -> 내 정보 받아오기 실행   - Glide 라이브러리 추가 설치   - 토큰 직접 추가 : 임시 방편임.

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
@@ -7,9 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.neppplus.retrofitlibrarytest_20211122.R
 import com.neppplus.retrofitlibrarytest_20211122.databinding.FragmentMyProfileBinding
+import com.neppplus.retrofitlibrarytest_20211122.datas.BasicResponse
 import com.neppplus.retrofitlibrarytest_20211122.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileFragment : BaseFragment() {
 
@@ -35,6 +40,32 @@ class MyProfileFragment : BaseFragment() {
     }
 
     override fun setValues() {
+
+        getMyInfoFromServer()
+    }
+
+    fun getMyInfoFromServer() {
+
+        apiService.getRequestMyInfo( ContextUtil.getToken(mContext) ).enqueue( object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    val br = response.body()!!
+
+                    binding.txtNickname.text = br.data.user.nickname
+                    Glide.with(mContext).load(br.data.user.profileImageURL).into(binding.imgProfile)
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 
