747bcd2a6b0b7ac75ffbabe2d7b0ee59b4073c6e
nepp-plus <nepp.plus@gmail.com>
2021-11-24 11:18:21 +0900

    products JSONArray 파싱  - 내부 데이터는 ProductData를 활용

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/DataResponse.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/DataResponse.kt
@@ -2,6 +2,10 @@ package com.neppplus.retrofitlibrarytest_20211122.datas
 
 class DataResponse(
     var user: UserData,
-    var token: String
+    var token: String,
+
+//    이 변수는 상품 목록에서만 사용.
+    var products: List<ProductData>
+
 ) {
 }
