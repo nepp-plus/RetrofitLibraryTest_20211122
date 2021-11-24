2b1c4798b4ccda8260ba6caa3d9f0089e5d25547
nepp-plus <nepp.plus@gmail.com>
2021-11-24 09:45:34 +0900

    본인 프로필 프래그먼트 kt 추가

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.neppplus.retrofitlibrarytest_20211122.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.neppplus.retrofitlibrarytest_20211122.R
+import com.neppplus.retrofitlibrarytest_20211122.databinding.FragmentMyProfileBinding
+
+class MyProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentMyProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_my_profile, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+}
