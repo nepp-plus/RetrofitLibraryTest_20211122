9269dfcd52d26127794e85618133d29c94523538
nepp-plus <nepp.plus@gmail.com>
2021-11-24 16:45:50 +0900

    로딩화면 추가

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.neppplus.retrofitlibrarytest_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
