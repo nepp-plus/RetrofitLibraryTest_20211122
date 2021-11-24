f6d621959528b4ee6189ba5ad50c587dd7e1c753
nepp-plus <nepp.plus@gmail.com>
2021-11-24 09:57:21 +0900

    뷰페이저 어댑터 / 화면의 뷰페이저 연결

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/MainActivity.kt
@@ -3,11 +3,14 @@ package com.neppplus.retrofitlibrarytest_20211122
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofitlibrarytest_20211122.adapters.MainViewPagerAdapter
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    lateinit var mvpa : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mvpa = MainViewPagerAdapter( supportFragmentManager )
+        binding.mainViewPager.adapter = mvpa
 
     }
 }
