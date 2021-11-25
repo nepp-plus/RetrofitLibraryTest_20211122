3b80100d5fddc2f5a97030a9cdb930380c49e53b
nepp-plus <nepp.plus@gmail.com>
2021-11-25 11:45:24 +0900

    리싸이클러뷰 - Intent로 화면이동

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
@@ -0,0 +1,26 @@
+package com.neppplus.retrofitlibrarytest_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityEditReviewBinding
+
+class EditReviewActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditReviewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_review)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/ProductRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/ProductRecyclerAdapter.kt
@@ -1,16 +1,15 @@
 package com.neppplus.retrofitlibrarytest_20211122.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.neppplus.retrofitlibrarytest_20211122.EditReviewActivity
 import com.neppplus.retrofitlibrarytest_20211122.R
 import com.neppplus.retrofitlibrarytest_20211122.datas.ProductData
 
@@ -24,6 +23,7 @@ class ProductRecyclerAdapter(val mContext: Context, val mList: List<ProductData>
         val txtStoreName = view.findViewById<TextView>(R.id.txtStoreName)
         val txtProductName = view.findViewById<TextView>(R.id.txtProductName)
         val txtProductPrice = view.findViewById<TextView>(R.id.txtProductPrice)
+        val btnWriteReview = view.findViewById<Button>(R.id.btnWriteReview)
 
         fun bind( data: ProductData ) {
 
@@ -52,6 +52,14 @@ class ProductRecyclerAdapter(val mContext: Context, val mList: List<ProductData>
                 alert.show()
 
                 return@setOnLongClickListener true
+            }
+
+            btnWriteReview.setOnClickListener {
+
+//                리뷰 작성 화면 이동
+                val myIntent = Intent(mContext, EditReviewActivity::class.java)
+                mContext.startActivity(myIntent)
+
             }
 
 
