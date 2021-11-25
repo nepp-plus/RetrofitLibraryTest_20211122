554178bd5c7d466b35ae52cc3cab81e7ffeaf58d
nepp-plus <nepp.plus@gmail.com>
2021-11-25 09:56:30 +0900

    닉네임 변경 기능 구현 예시 (1)   - 얼럿창 띄우기 + 내부 xml 별도 작성

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.retrofitlibrarytest_20211122.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -38,6 +40,24 @@ class MyProfileFragment : BaseFragment() {
     }
 
     override fun setupEvents() {
+
+        binding.btnEditNickname.setOnClickListener {
+
+//            닉네임 변경 입력 (AlertDialog 커스텀뷰) + API 호출
+
+            val alert = AlertDialog.Builder(mContext)
+
+            alert.setMessage("닉네임 변경")
+
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+
+
+        }
 
     }
 
