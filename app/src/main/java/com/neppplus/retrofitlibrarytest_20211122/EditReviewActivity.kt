ec9ac0f308b05571011555021ef22fead2273aaa
nepp-plus <nepp.plus@gmail.com>
2021-11-25 12:03:15 +0900

    리뷰작성 - 기본 정보 표시 완료

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
@@ -6,6 +6,8 @@ import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityEditReviewBinding
 import com.neppplus.retrofitlibrarytest_20211122.datas.ProductData
 import com.neppplus.retrofitlibrarytest_20211122.utils.GlobalData
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditReviewActivity : BaseActivity() {
 
@@ -30,6 +32,17 @@ class EditReviewActivity : BaseActivity() {
 
         binding.txtProductName.text =  mProductData.name
         binding.txtUserNickname.text =  GlobalData.loginUser!!.nickname
+
+//        오늘 날짜 -> 2021.5.5 형태로 가공 -> 텍스트뷰에 반영
+
+//        1. 오늘 날짜?
+        val now = Calendar.getInstance()  // 현재 일시 자동 기록
+
+//        원하는 형태로 가공 (String 생성)
+        val sdf = SimpleDateFormat( "yyyy.M.d" )
+        val nowString = sdf.format( now.time )
+
+        binding.txtToday.text = nowString
 
     }
 }
