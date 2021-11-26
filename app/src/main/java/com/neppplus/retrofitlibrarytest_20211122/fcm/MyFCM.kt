6375db332548b8350f9af1ea45532125db6b5b58
nepp-plus <nepp.plus@gmail.com>
2021-11-26 13:03:05 +0900

    푸시알림 수신 이벤트 담당 서비스 클래스 추가  - Manifest에서 푸시알림이 오면 담당하도록 세팅 (intent-filter)

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fcm/MyFCM.kt
@@ -0,0 +1,15 @@
+package com.neppplus.retrofitlibrarytest_20211122.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFCM : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+//        실제 푸시 신호가 왔을때 할 행동 - p0변수 : 어떤 메세지? 담아주는 역할
+
+    }
+
+}
