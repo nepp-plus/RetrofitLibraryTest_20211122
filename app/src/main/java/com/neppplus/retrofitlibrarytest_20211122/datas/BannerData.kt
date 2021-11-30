e2f2eec8e5d4c05c8900f644b00be7ec0ee145d6
nepp-plus <nepp.plus@gmail.com>
2021-11-30 15:04:29 +0900

    하나의 배너 정보를 받아서   - 그림 표시 / 클릭시 웹 링크 기능 구현   - 프래그먼트 종류 구현

---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/BannerData.kt
@@ -0,0 +1,13 @@
+package com.neppplus.retrofitlibrarytest_20211122.datas
+
+import com.google.gson.annotations.SerializedName
+
+class BannerData(
+    var id: Int,
+    @SerializedName("img_url")
+    var displayImageUrl: String,
+    @SerializedName("click_url")
+    var clickUrl: String
+) {
+
+}
